6980d8871eb3b05f42df4c5fc2ba96a346d516f1
SoundChaser83 <sportfreak83@yahoo.com>
2021-12-23 14:53:37 -0800

    Reduced item levels of Griswold's premium shop

--- Source/items.cpp
+++ Source/items.cpp
@@ -311,32 +311,32 @@ _sfx_id ItemDropSnds[] = {
 /** Maps from Griswold premium item number to a quality level delta as added to the base quality level. */
 int premiumlvladd[] = {
 	// clang-format off
-	-1,
-	-1,
-	 0,
-	 0,
-	 1,
-	 2,
+	-5,
+	-5,
+	-5,
+	-5,
+	-5,
+	-5,
 	// clang-format on
 };
 /** Maps from Griswold premium item number to a quality level delta as added to the base quality level. */
 int premiumLvlAddHellfire[] = {
 	// clang-format off
-	-1,
-	-1,
-	-1,
-	 0,
-	 0,
-	 0,
-	 0,
-	 1,
-	 1,
-	 1,
-	 1,
-	 2,
-	 2,
-	 3,
-	 3,
+	-5,
+	-5,
+	-5,
+	-5,
+	-5,
+	-5,
+	-5,
+	-5,
+	-5,
+	-5,
+	-5,
+	-5,
+	-5,
+	-5,
+	-5,
 	// clang-format on
 };
 
